5fa6de182fe16f7e430fdb76214a212ecbb0d035
Alvin Son <159491405+alvocadoo@users.noreply.github.com>
2024-05-23 21:01:49 -0700

    Update crowdfunding_db_schema.sq.sql

--- crowdfunding_db_schema.sq.sql
+++ crowdfunding_db_schema.sq.sql
@@ -7,21 +7,21 @@ DROP TABLE if exists campaign;
 
 
 CREATE TABLE category(
-	category_id VARCHAR(40) PRIMARY KEY,
-	category VARCHAR(40) NOT NULL
+	category_id VARCHAR(60) PRIMARY KEY,
+	category VARCHAR(60) NOT NULL
 ); 
 
 
 CREATE TABLE subcategory(
-	subcategory_id VARCHAR(40) PRIMARY KEY,
-	subcategory VARCHAR(40) NOT NULL
+	subcategory_id VARCHAR(60) PRIMARY KEY,
+	subcategory VARCHAR(60) NOT NULL
 );
 
 
 CREATE TABLE contacts(
 	contact_id int PRIMARY KEY,
-	first_name VARCHAR(40) NOT NULL,
-	last_name VARCHAR(40) NOT NULL,
+	first_name VARCHAR(60) NOT NULL,
+	last_name VARCHAR(60) NOT NULL,
 	email VARCHAR(60) NOT NULL
 );
 
@@ -29,18 +29,18 @@ CREATE TABLE contacts(
 CREATE TABLE campaign(
 	cf_id int PRIMARY KEY,
 	contact_id int NOT NULL,
-	company_name VARCHAR(40) NOT NULL,
-	description VARCHAR (400) NOT NULL,
+	company_name VARCHAR(60) NOT NULL,
+	description VARCHAR (600) NOT NULL,
 	goal NUMERIC(10,2) NOT NULL,
 	pledged NUMERIC(10,2) NOT NULL,
-	outcome VARCHAR (40) NOT NULL,
+	outcome VARCHAR (60) NOT NULL,
 	backers_count int NOT NULL,
-	country VARCHAR (40) NOT NULL,
-	currency VARCHAR (40) NOT NULL,
+	country VARCHAR (60) NOT NULL,
+	currency VARCHAR (4600) NOT NULL,
 	launched_date DATE NOT NULL,
 	end_date DATE NULL,
-	category_id VARCHAR (40) NOT NULL,
-	subcategory_id VARCHAR (40) NOT NULL,
+	category_id VARCHAR (60) NOT NULL,
+	subcategory_id VARCHAR (60) NOT NULL,
 	FOREIGN KEY (category_id) REFERENCES category (category_id),
 	FOREIGN KEY (subcategory_id) REFERENCES subcategory (subcategory_id),
 	FOREIGN KEY (contact_id) REFERENCES contacts (contact_id)
